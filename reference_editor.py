5c52946d9c903f5a870d234d4bde752f923ed619
Mehrdad Bakhtiari <mehrdad.baxtiari@gmail.com>
2017-04-05 20:37:49 -0700

    add reference_editor

---
+++ reference_editor.py
@@ -0,0 +1,22 @@
+from Bio import Seq, SeqRecord, SeqIO
+
+
+def add_two_copy_to_all_patterns(patterns, start_points):
+    file_name = 'chr15.fa'
+    fasta_sequences = SeqIO.parse(open(file_name), 'fasta')
+    sequence = ''
+    record = SeqRecord.SeqRecord('')
+    for fasta in fasta_sequences:
+        record = fasta
+        name, sequence = fasta.id, str(fasta.seq)
+    total_added_chars = 0
+    for i in range(len(patterns)):
+        start_point = start_points[i] + total_added_chars
+        pattern = patterns[i]
+        sequence = sequence[:start_point] + pattern * 2 + sequence[start_point:]
+        total_added_chars += len(pattern * 2)
+
+    record.seq = Seq.Seq(sequence)
+    output_name = 'edited_chr15_two_more_copies.fa'
+    with open(output_name, 'w') as output_handle:
+        SeqIO.write([record], output_handle, 'fasta')
