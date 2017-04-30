343637385d45612912a878709eb4713f71bf3996
Mehrdad Bakhtiari <mehrdad.baxtiari@gmail.com>
2017-04-29 21:15:07 -0700

    add ReferenceVNTR class which works with different chromosomes

--- reference_repeat_finder.py
+++ reference_repeat_finder.py
@@ -2,50 +2,62 @@ from Bio import SeqIO
 from hmm_utils import is_matching_state, build_reference_repeat_finder_hmm
 
 
-def get_scores_and_segments(corresponding_region_in_ref, patterns, copies=40):
-    model = build_reference_repeat_finder_hmm(patterns, copies=copies)
-    single_model = build_reference_repeat_finder_hmm(patterns, copies=1)
-    logp, path = model.viterbi(corresponding_region_in_ref)
-    visited_states = [state.name for idx, state in path[1:-1]]
+class ReferenceVNTR:
+    def __init__(self, pattern, start_point, estimated_repeats, chromosome='chr15'):
+        self.pattern = pattern
+        self.start_point = start_point
+        self.estimated_repeats = estimated_repeats
+        self.chromosome = chromosome.lower()
 
-    lengths = []
-    prev_start = None
-    for i in range(len(visited_states)):
-        if visited_states[i].startswith('unit_end') and prev_start is not None:
-            current_len = 0
-            for j in range(prev_start, i):
-                if is_matching_state(visited_states[j]):
-                    current_len += 1
-            lengths.append(current_len)
-        if visited_states[i].startswith('unit_start'):
-            prev_start = i
+    def get_scores_and_segments(self, corresponding_region_in_ref):
+        patterns = [self.pattern]
+        model = build_reference_repeat_finder_hmm(patterns, copies=self.estimated_repeats)
+        single_model = build_reference_repeat_finder_hmm(patterns, copies=1)
+        logp, path = model.viterbi(corresponding_region_in_ref)
+        visited_states = [state.name for idx, state in path[1:-1]]
 
-    repeat_segments = []
-    scores = []
-    added = 0
-    for l in lengths:
-        repeat_segments.append(corresponding_region_in_ref[added:added+l])
-        score, temp_path = single_model.viterbi(corresponding_region_in_ref[added:added+l])
-        scores.append(score)
-        added += l
-    return scores, repeat_segments, visited_states
+        lengths = []
+        prev_start = None
+        for i in range(len(visited_states)):
+            if visited_states[i].startswith('unit_end') and prev_start is not None:
+                current_len = 0
+                for j in range(prev_start, i):
+                    if is_matching_state(visited_states[j]):
+                        current_len += 1
+                lengths.append(current_len)
+            if visited_states[i].startswith('unit_start'):
+                prev_start = i
 
+        repeat_segments = []
+        scores = []
+        added = 0
+        for l in lengths:
+            repeat_segments.append(self, corresponding_region_in_ref[added:added+l])
+            score, temp_path = single_model.viterbi(corresponding_region_in_ref[added:added+l])
+            scores.append(score)
+            added += l
+        return scores, repeat_segments, visited_states
 
-def find_number_of_tandem_repeats_in_reference(pattern, pattern_start, copies, ref_file_name='chr15.fa'):
-    fasta_sequences = SeqIO.parse(open(ref_file_name), 'fasta')
-    ref_sequence = ''
-    for fasta in fasta_sequences:
-        name, ref_sequence = fasta.id, str(fasta.seq)
-    corresponding_region_in_ref = ref_sequence[pattern_start:pattern_start + len(pattern) * copies].upper()
+    def get_corresponding_region_in_ref(self):
+        ref_file_name = self.chromosome + '.fa'
+        fasta_sequences = SeqIO.parse(open(ref_file_name), 'fasta')
+        ref_sequence = ''
+        for fasta in fasta_sequences:
+            name, ref_sequence = fasta.id, str(fasta.seq)
+        estimated_length = len(self.pattern) * self.estimated_repeats
+        corresponding_region_in_ref = ref_sequence[self.start_point:self.start_point + estimated_length].upper()
+        return corresponding_region_in_ref
 
-    original_scores, repeat_segments, states = get_scores_and_segments(corresponding_region_in_ref, [pattern], copies)
-    hmm = build_reference_repeat_finder_hmm(repeat_segments, copies=1)
-    scores = []
-    for seg in repeat_segments:
-        score, temp_path = hmm.viterbi(seg)
-        scores.append(score)
+    def find_number_of_tandem_repeats_in_reference(self):
+        corresponding_region_in_ref = self.get_corresponding_region_in_ref()
+        original_scores, repeat_segments, states = self.get_scores_and_segments(corresponding_region_in_ref)
+        hmm = build_reference_repeat_finder_hmm(repeat_segments, copies=1)
+        scores = []
+        for seg in repeat_segments:
+            score, temp_path = hmm.viterbi(seg)
+            scores.append(score)
 
-    return len(repeat_segments), repeat_segments, states
+        return len(repeat_segments), repeat_segments, states
 
 
 def plot_segment_scores(scores, pattern):
@@ -75,11 +87,12 @@ def find_true_repeat_counts():
     skipped_vntrs = []
     for i in range(len(patterns)):
         print(i)
-        copies = vntrseek_repeats[i] + 2
-        if i < len(patterns) - 1 and len(patterns[i]) * copies + start_points[i] > start_points[i+1]:
-            copies += vntrseek_repeats[i+1]
-        copies += 3
-        repeat_count, repeat_segments, states = find_number_of_tandem_repeats_in_reference(patterns[i], start_points[i], copies)
+        estimated_repeats = vntrseek_repeats[i] + 2
+        if i < len(patterns) - 1 and len(patterns[i]) * estimated_repeats + start_points[i] > start_points[i+1]:
+            estimated_repeats += vntrseek_repeats[i+1]
+        estimated_repeats += 3
+        reference_vntr = ReferenceVNTR(patterns[i], start_points[i], estimated_repeats)
+        repeat_count, repeat_segments, states = reference_vntr.find_number_of_tandem_repeats_in_reference()
         repeats.append(repeat_count)
         if i in skipped_vntrs:
             repeat_count = 0
