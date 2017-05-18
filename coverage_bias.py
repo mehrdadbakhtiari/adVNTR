33c00fc205656a7a1f7db2db4b2ba10d6f477a2a
Mehrdad Bakhtiari <mehrdad.baxtiari@gmail.com>
2017-05-17 19:11:04 -0700

    add coverage bias file and classes

---
+++ coverage_bias.py
@@ -0,0 +1,11 @@
+from settings import *
+
+
+class CoverageBiasDetector:
+    """Find the coverage distribution based on GC content."""
+    pass
+
+
+class CoverageCorrector:
+    """Normalize the coverage based on the coverage distribution."""
+    pass
--- settings.py
+++ settings.py
@@ -5,3 +5,5 @@ USE_TRAINED_HMMS = True
 TRAINED_HMMS_DIR = 'trained_HMMs/'
 
 BLAST_TMP_DIR = 'blast_tmp/'
+
+GC_CONTENT_WINDOW_SIZE = 100
