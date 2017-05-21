0898878089728553ed2c542eb49ad87ac0f7d170
Mehrdad Bakhtiari <mehrdad.baxtiari@gmail.com>
2017-05-20 22:48:34 -0700

    find coverage for each window

--- coverage_bias.py
+++ coverage_bias.py
@@ -1,11 +1,15 @@
 from settings import *
 from utils import get_chromosome_reference_sequence, get_gc_content
+import pysam
 
 
 class CoverageBiasDetector:
     """Find the coverage distribution based on GC content."""
 
     def __init__(self, sam_file='original_reads/paired_dat.sam'):
+        self.sam_file = sam_file
+
+    def get_sam_file_from_read_files(self):
         pass
 
     def get_gc_contents_of_reference_windows(self):
@@ -20,8 +24,24 @@ class CoverageBiasDetector:
                 gc_map[chromosome][window_number] = gc_content
         return gc_map
 
+    def add_bp_to_coverage_map(self, covered_bps, window_number, read_start, read_end):
+        start = max(window_number * GC_CONTENT_WINDOW_SIZE, read_start)
+        end = min(window_number * GC_CONTENT_WINDOW_SIZE + GC_CONTENT_WINDOW_SIZE, read_end)
+        if window_number not in covered_bps:
+            covered_bps[window_number] = 0
+            covered_bps[window_number] += end - start
+        if read_end > window_number * GC_CONTENT_WINDOW_SIZE + GC_CONTENT_WINDOW_SIZE:
+            self.add_bp_to_coverage_map(covered_bps, window_number+1, end, read_end)
+
     def get_gc_content_coverage_map(self):
         reference_gc_map = self.get_gc_contents_of_reference_windows()
+        samfile = pysam.AlignmentFile(self.sam_file, "r")
+        covered_bps = {}
+        for chromosome in CHROMOSOMES:
+            covered_bps[chromosome] = {}
+        for read in samfile.fetch():
+            window_number = read.reference_start / GC_CONTENT_WINDOW_SIZE
+            self.add_bp_to_coverage_map(covered_bps, window_number, read.reference_start, read.reference_end)
 
 
 class CoverageCorrector:
