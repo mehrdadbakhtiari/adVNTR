57db4f7de86f1df7cd0ca622ea506581ae36314c
Mehrdad Bakhtiari <mehrdad.baxtiari@gmail.com>
2017-06-14 23:15:26 -0700

    fix score distribution

--- vntr_finder.py
+++ vntr_finder.py
@@ -80,9 +80,9 @@ class VNTRFinder:
         return blast_ids
 
     @staticmethod
-    def add_hmm_score_to_list(self, semaphore, list_lock, hmm, read, result_scores):
-        list_lock.acquire()
+    def add_hmm_score_to_list(semaphore, list_lock, hmm, read, result_scores):
         logp, vpath = hmm.viterbi(str(read.seq))
+        list_lock.acquire()
         result_scores.append(logp)
         list_lock.release()
         semaphore.release()
@@ -94,7 +94,7 @@ class VNTRFinder:
         semaphore = Semaphore(settings.CORES)
         list_lock = Lock()
         process_list = []
-        false_reads_score = []
+        false_scores = []
         read_mode = 'r' if alignment_file.endswith('sam') else 'rb'
         samfile = pysam.AlignmentFile(alignment_file, read_mode)
         for read in samfile.fetch():
@@ -115,13 +115,13 @@ class VNTRFinder:
             if read.seq.count('N') > 0:
                 continue
             semaphore.acquire()
-            p = Process(target=self.add_hmm_score_to_list, args=(semaphore, list_lock, hmm, read, false_reads_score))
+            p = Process(target=VNTRFinder.add_hmm_score_to_list, args=(semaphore, list_lock, hmm, read, false_scores))
             process_list.append(p)
             p.start()
         for p in process_list:
             p.join()
 
-        score = numpy.percentile(false_reads_score, 100 - 0.0001)
+        score = numpy.percentile(false_scores, 100 - 0.0001)
         return score
 
     def get_min_score_to_select_a_read(self, hmm, alignment_file, read_length):
