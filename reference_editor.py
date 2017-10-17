a448c0192e2fbeeb00f0bc905de5d48ad9bfde7f
Mehrdad Bakhtiari <mehrdad.baxtiari@gmail.com>
2017-10-17 14:06:36 -0700

    basic frameshifted region creator

--- reference_editor.py
+++ reference_editor.py
@@ -23,6 +23,28 @@ def add_two_copy_to_all_patterns(patterns, start_points):
         SeqIO.write([record], output_handle, 'fasta')
 
 
+def create_cel_frameshifts(cel_vntr):
+    sequence = get_chromosome_reference_sequence(cel_vntr.chromosome)
+    left_flank = sequence[cel_vntr.start_point-3000:cel_vntr.start_point]
+    vntr_end = cel_vntr.start_point + cel_vntr.get_length()
+    vntr = sequence[cel_vntr.start_point:vntr_end]
+    right_flank = sequence[vntr_end:vntr_end+3000]
+
+    for frameshift in xrange(160, 360, 10):
+        deletion = left_flank + vntr[:frameshift] + vntr[frameshift+1:] + right_flank
+        insertion = left_flank + vntr[:frameshift+5] + 'C' + vntr[frameshift+5:] + right_flank
+        record = SeqRecord.SeqRecord('')
+        record.seq = Seq.Seq(deletion)
+        deletion_output_name = 'chr9_cel_deletion_%s.fa' % str(frameshift)
+        insertion_output_name = 'chr9_cel_insertion_%s.fa' % str(frameshift + 5)
+        with open(deletion_output_name, 'w') as output_handle:
+            SeqIO.write([record], output_handle, 'fasta')
+        record.seq = Seq.Seq(insertion)
+        with open(insertion_output_name, 'w') as output_handle:
+            SeqIO.write([record], output_handle, 'fasta')
+
+
+
 def create_reference_region_with_specific_repeats(reference_vntr, desired_repeats, output_name):
     record = SeqRecord.SeqRecord('')
     sequence = get_chromosome_reference_sequence(reference_vntr.chromosome)
