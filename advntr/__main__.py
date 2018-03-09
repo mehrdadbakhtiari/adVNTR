fc86e94a4be1c2f993e574cecaaddaedc4ca1b31
Mehrdad Bakhtiari <mehrdad.baxtiari@gmail.com>
2018-03-09 14:02:41 -0800

    new formatter for argparse

--- advntr/__main__.py
+++ advntr/__main__.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# from __future__ import absolute_import
-
 import argparse
 
 from advntr.advntr_commands import genotype, view_model, not_implemented_command
 from advntr import settings
+
+
+class CustomHelpFormatter(argparse.HelpFormatter):
+    def __init__(self, prog):
+        super(CustomHelpFormatter, self).__init__(prog, max_help_position=40, width=80)
+
+    def _format_action_invocation(self, action):
+        default = self._metavar_formatter(action, action.dest)
+        args_string = self._format_args(action, default)
+        return '/'.join(action.option_strings) + ' ' + args_string
 
 
 def run_advntr():
@@ -22,39 +30,41 @@ def run_advntr():
            '         delmodel\tremove a model from database\n'
 
     usage = '\r{}\nusage: %(prog)s <command> [options]\n\n\r{}\r{}'.format(description.ljust(len('usage:')), help, '\n')
-    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, usage=usage, add_help=False)
+    parser = argparse.ArgumentParser(usage=usage, add_help=False)
     subparsers = parser.add_subparsers(title='Commands', dest='command')
 
-    genotype_parser = subparsers.add_parser('genotype', usage='advntr genotype [options]')
+    fmt = lambda prog: CustomHelpFormatter(prog)
+    genotype_parser = subparsers.add_parser('genotype', usage='advntr genotype [options]', formatter_class=fmt)
     genotype_parser.add_argument('-a', '--alignment_file', type=str, help='Alignment file in BAM format or SAM format',
-                                 metavar='FILE')
-    genotype_parser.add_argument('-f', '--fasta', type=str, help='Fasta file containing raw reads', metavar='FILE')
+                                 metavar='<file>')
+    genotype_parser.add_argument('-f', '--fasta', type=str, help='Fasta file containing raw reads', metavar='<file>')
     genotype_parser.add_argument('-fs', '--frameshift', action='store_true',
-                                 help='Set this flag to search for frameshifts in VNTR instead of copy number.'
+                                 help='set this flag to search for frameshifts in VNTR instead of copy number.'
                                  '\n    * Supported VNTR IDs: %s' % settings.FRAMESHIFT_VNTRS)
     genotype_parser.add_argument('-e', '--expansion', action='store_true',
-                                 help='Set this flag to determine long expansion from PCR-free data')
-    genotype_parser.add_argument('-c', '--coverage', type=float, metavar='<COVERAGE>',
-                                 help='Average sequencing coverage in PCR-free sequencing')
+                                 help='set this flag to determine long expansion from PCR-free data')
+    genotype_parser.add_argument('-c', '--coverage', type=float, metavar='<float>',
+                                 help='average sequencing coverage in PCR-free sequencing')
     genotype_parser.add_argument('-p', '--pacbio', action='store_true',
-                                 help='Set this flag if input file contains PacBio reads instead of Illumina reads')
+                                 help='set this flag if input file contains PacBio reads instead of Illumina reads')
     genotype_parser.add_argument('-n', '--nanopore', action='store_true',
-                                 help='Set this flag if input file contains Nanopore MinION reads instead of Illumina')
-    genotype_parser.add_argument('-wd', '--working_directory', type=str, metavar='<DIRECTORY>',
-                                 help='Working directory for creating temporary files needed for computation')
-    genotype_parser.add_argument('-m', '--models', type=str, metavar='<FILE>', default='vntr_data/hg19_VNTRs.db',
-                                 help='File containing VNTRs information [%(default)s]')
-    genotype_parser.add_argument('-t', '--threads', type=int, metavar='<nthreads>', default=4,
-                                 help='Run the tool on <nthreads> parallel threads which will run on separate processors/cores [%(default)s]')
-    genotype_parser.add_argument('-vid', '--vntr_id', type=str, metavar='<VNTR ID>', default=None,
-                                 help='Comma-separated list of VNTR IDs')
+                                 help='set this flag if input file contains Nanopore MinION reads instead of Illumina')
+    genotype_parser.add_argument('--working_directory', type=str, metavar='<path>',
+                                 help='working directory for creating temporary files needed for computation')
+    genotype_parser.add_argument('-m', '--models', type=str, metavar='<file>', default='vntr_data/hg19_VNTRs.db',
+                                 help='file containing VNTRs information [%(default)s]')
+    genotype_parser.add_argument('-t', '--threads', type=int, metavar='<int>', default=4,
+                                 help='number of threads [%(default)s]')
+    genotype_parser.add_argument('-vid', '--vntr_id', type=str, metavar='<text>', default=None,
+                                 help='comma-separated list of VNTR IDs')
     genotype_parser.add_argument('-naive', '--naive', action='store_true', default=False,
-                                 help='Use naive approach for PacBio reads')
+                                 help='use naive approach for PacBio reads')
 
-    viewmodel_parser = subparsers.add_parser('viewmodel', usage='advntr viewmodel [options]')
-    viewmodel_parser.add_argument('-g', '--gene', type=str, default='', help='Comma-separated list of Gene Names')
-    viewmodel_parser.add_argument('-p', '--pattern', type=str, default=None,
-                                  help='Repeating pattern of VNTR in forward (5\' to 3\') direction')
+    viewmodel_parser = subparsers.add_parser('viewmodel', usage='advntr viewmodel [options]', formatter_class=fmt)
+    viewmodel_parser.add_argument('-g', '--gene', type=str, default='', metavar='<text>',
+                                  help='comma-separated list of Gene Names')
+    viewmodel_parser.add_argument('-p', '--pattern', type=str, default=None, metavar='<text>',
+                                  help='repeating pattern of VNTR in forward (5\' to 3\') direction')
 
     addmodel_parser = subparsers.add_parser('addmodel', usage='advntr addmodel [options]')
     delmodel_parser = subparsers.add_parser('delmodel', usage='advntr delmodel [options]')
