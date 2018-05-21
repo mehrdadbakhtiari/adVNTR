6f622d8eaa5c717aa37dab9e0e6ada0ef320bbc6
Mehrdad Bakhtiari <mehrdad.baxtiari@gmail.com>
2018-05-20 21:44:38 -0700

    use scikit-learn instead of sklearn

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='advntr',
       url='https://github.com/mehrdadbakhtiari/adVNTR',
       packages=['advntr', 'pomegranate'],
       package_dir={'advntr': 'advntr', 'advntr.pomegranate': 'pomegranate'},
-      install_requires=['networkx==1.11', 'scipy', 'biopython', 'cython', 'sklearn'],
+      install_requires=['networkx==1.11', 'scipy', 'biopython', 'cython', 'scikit-learn'],
       provides=["advntr"],
       entry_points={
             'console_scripts': ['advntr=advntr.__main__:main']
