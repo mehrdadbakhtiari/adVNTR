3eae9c1d86fa29ea9c2e71e813a1fc802aa9a0e1
milad mortazavi <milad@cascadetechnologies.com>
2019-04-27 22:10:26 -0700

    Some changes to the State and Model classes

--- hmm/hmm.py
+++ hmm/hmm.py
@@ -9,16 +9,9 @@ class DiscreteDistribution:
 
 class State:
 
-    def __init__(self, name=None):
-        self.name = str(name)
-        self.outgoing_edges = []
-
-    def add_outgoing_edge(self, state):
-        self.outgoing_edges.append(state)
-
-    # State has all the outgoing edges
-    edges = dict()
-
+    def __init__(self, distribution=None, name=None):
+        self.distribution = distribution
+        self.name = name or str(id(self))
 
 class Model:
     """ Hidden Markov Model """
@@ -28,34 +21,63 @@ class Model:
         self.name = str(name) or str(id(self))
         self.model = "HiddenMarkovModel"
 
-        self.start = start or State(name=self.name + "-start")
-        self.end = end or State(name=self.name + "-end")
+        # states
+        self.states   = []
+        self.n_states = 0
+        self.start    = start or State(name=self.name + "-start")
+        self.end      = end or State(name=self.name + "-end")
 
-        self.number_of_edges = 0
-        self.number_of_states = 0
+        # Put start and end in the states
+        self.add_states(self.start, self.end)
 
-        # Put start and end in the state
-        self.states = set()
-        self.states.add(self.start)
-        self.states.add(self.end)
+        # edges
+        self.edges    = []
+        self.n_edges  = 0
 
         # 2D matrix (After conforming the topology, create one matrix for visualization)
         self.transition_matrix = None
 
-        self.states = set()
         self.dynamic_table = []
 
     def add_model(self, model):
         pass
 
     def add_state(self, state):
-        pass
+        self.states.append(state)
+        self.n_states += 1
 
-    def add_states(self, states):
-        pass
+    def add_states(self, *states):
+        for state in states:
+            self.add_state(state)
 
-    def states(self):
-        pass
+    def add_edge(self, a, b):
+        self.edges.append( ( a, b ) )
+        self.n_edges += 1
+
+    def add_transition( self, a, b ):
+        self.add_edge( a, b )
+
+    def state_count(self):
+        return self.n_states
+
+    def edge_count(self):
+        return self.n_edges
+
+    def dense_transition_matrix(self):
+        """
+        Returns the dense transition matrix. Useful if the transitions of
+        somewhat small models need to be analyzed.
+        """
+
+        m = len(self.states)
+        transition_log_probabilities = numpy.zeros( (m, m) ) + NEGINF
+
+        for i in range(m):
+          for n in range( self.out_edge_count[i], self.out_edge_count[i+1] ):
+            transition_log_probabilities[i, self.out_transitions[n]] = \
+              self.out_transition_log_probabilities[n]
+
+        return transition_log_probabilities
 
     def add_transition(self, from_state, to_state, probability, pseudocount):
         pass
@@ -84,7 +106,7 @@ class Model:
     def concatenate(self,model):
         pass
   
-    def fit(self,fit_patterns, algorithm='viterbi', transition_pseudocount=1, use_pseudocount=True)
+    def fit(self,fit_patterns, algorithm='viterbi', transition_pseudocount=1, use_pseudocount=True):
         pass
 
 if __name__ == "__main__":
--- hmm/test_hmm.py
+++ hmm/test_hmm.py
@@ -2,27 +2,53 @@ from collections import defaultdict
 
 from hmm import State
 from hmm import Model
+from hmm import DiscreteDistribution
 
 def test_hmm_add_transition():
+    pass
+
+def test_hmm_distribution():
     pass
 
 def test_hmm_state():
     transitions = defaultdict(lambda: defaultdict(float))
     emissions = defaultdict(lambda: defaultdict(float))
 
-    hmm = Model(name="HiddenMarkovModel")
-
     # State(insert_distribution, name='I%s_%s' % (i, repeat))
-    state1 = State(name="intron")
-    state2 = State(name="exon")
+    distribution = {'A':0.2, 'C':0.3, 'G':0.3, 'T':0.2}
+    state1 = State(distribution, name="intron")
+    state2 = State(distribution)
 
     emissions['start']['intron'] = 0.5
     emissions['start']['exon'] = 0.5
     emissions['intron']['intron'] = 0.6
     emissions['intron']['exon'] = 0.4
     emissions['exon']['intron'] = 0.6
-    emissions['exon']['exon'] = 0.6
+    emissions['exon']['exon'] = 0.4
 
+    print("state1 name: ", state1.name)
+    print("state2 name: ", state2.name)
+    print("emissions: ", emissions)
+
+def test_hmm_model():
+    hmm = Model(name="HiddenMarkovModel")
+
+    distribution = {'A':0.2, 'C':0.3, 'G':0.3, 'T':0.2}
+    state1 = State(distribution, name="s1")
+    state2 = State(distribution, name="s2")
+
+    hmm.add_states(state1, state2)
+    hmm.add_edge(hmm.start, state1)
+    hmm.add_edge(state1, state2)
+    hmm.add_edge(state2, hmm.start)
+
+    print("states: ", hmm.states)
+    for state in hmm.states:
+        print("state name: ", state.name)
+    print("n states: ", hmm.n_states)
+    print("n edges: ", hmm.n_edges)
+    pass
 
 if __name__ == "__main__":
-    test_hmm_add_transition()
+    test_hmm_state()
+    test_hmm_model()
