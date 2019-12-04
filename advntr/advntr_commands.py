037cff3b1dd63748af6306c8d015bbd8a2a68e06
Mehrdad Bakhtiari <mehrdad.baxtiari@gmail.com>
2019-12-03 16:55:36 -0800

    add logs

--- advntr/advntr_commands.py
+++ advntr/advntr_commands.py
@@ -104,6 +104,7 @@ def genotype(args, genotype_parser):
     target_vntrs = [ref_vntr.id for ref_vntr in reference_vntrs]
     if args.vntr_id is not None:
         target_vntrs = [int(vid) for vid in args.vntr_id.split(',')]
+    logging.info('Running adVNTR for %s VNTRs' % len(target_vntrs))
     genome_analyzier = GenomeAnalyzer(reference_vntrs, target_vntrs, working_directory, args.outfmt, args.haploid,
                                       args.reference_filename)
     if args.pacbio:
