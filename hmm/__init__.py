c01fde63bf44f9bed6d429f28aa9083cfb1d49cf
Jonghun Park <jonghunpark@ucsd.edu>
2020-05-28 17:14:14 -0700

    Set all environment varialbe necessary at runtime

--- hmm/__init__.py
+++ hmm/__init__.py
@@ -1,6 +1,33 @@
 
+import os
+import numpy
+import pyximport
 from .hmm import *
-import pyximport; pyximport.install()
+
+# Adapted from Cython docs
+# https://github.com/cython/cython/wiki/InstallingOnWindows#mingw--numpy--pyximport-at-runtime
+if os.name == 'nt':
+    if os.environ.has_key('CPATH'):
+        os.environ['CPATH'] = os.environ['CPATH'] + numpy.get_include()
+    else:
+        os.environ['CPATH'] = numpy.get_include()
+
+    # XXX: we're assuming that MinGW is installed in C:\MinGW (default)
+    if os.environ.has_key('PATH'):
+        os.environ['PATH'] = os.environ['PATH'] + ';C:\MinGW\bin'
+    else:
+        os.environ['PATH'] = 'C:\MinGW\bin'
+
+    mingw_setup_args = { 'options': { 'build_ext': { 'compiler': 'mingw32' } } }
+    pyximport.install(setup_args=mingw_setup_args)
+
+elif os.name == 'posix':
+    if os.environ.has_key('CFLAGS'):
+        os.environ['CFLAGS'] = os.environ['CFLAGS'] + ' -I' + numpy.get_include()
+    else:
+        os.environ['CFLAGS'] = ' -I' + numpy.get_include()
+
+    pyximport.install()
 
 try:
     from .base import *
