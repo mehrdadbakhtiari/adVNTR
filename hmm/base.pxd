9adf24b5f6944cbfc751ba07c3034592dffb89e0
Jonghun Park <jonghunpark@ucsd.edu>
2020-12-28 09:49:48 -0800

    Use log prob for emission probs

--- hmm/base.pxd
+++ hmm/base.pxd
@@ -1,11 +1,9 @@
 # base.pxd
 
-cimport numpy
-
 
 cdef class DiscreteDistribution(object):
     cdef public dict emission
-
+    cdef public dict log_emission
 
 cdef class State(object):
     cdef public DiscreteDistribution distribution
--- hmm/base.pyx
+++ hmm/base.pyx
@@ -1,11 +1,17 @@
+from libc.math cimport log
 
 cdef class DiscreteDistribution(object):
 
     def __init__(self, emission):
         self.emission = emission
 
+        key_map = {'A':0, 'C':1, 'G':2, 'T':3}
+        self.log_emission = {key_map[key]: log(value) for key, value in emission.items()}
+        # print(self.log_emission)
+
     def __getitem__(self, key):
-        return self.emission[chr(key)]
+        return self.log_emission[key]
+        # return self.emission[key]
 
 
 cdef class State(object):
